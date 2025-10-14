b0af7a1b2b420a72ca78f2a7b6000901f1aef374
Daniel Williams <danielfwilliams@protonmail.com>
2025-10-14 19:24:44 -0400

    Add boiler plate

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from sam-parser!")
+import argparse
+import os
+import requests
+import sys
+
+import dotenv
+
+
+dotenv.load_dotenv()
+
+SAM_API_KEY = os.getenv("SAM_API_KEY")
+SAM_ENDPOINT = "https://api.data.gov/sam/v1/registrations/"
+
+
+def query_sam_api(query: str) -> list[dict]:
+    url = f"{SAM_ENDPOINT}?api_key={SAM_API_KEY}?qterms={query}"
+    response = requests.get(url)
+    return response.json()
+
+
+def main() -> int:
+    parser = _config_parser()
+    _ = parser.parse_args()
+
+    return 0
+
+
+def _config_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="sam-parser")
+    return parser
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
